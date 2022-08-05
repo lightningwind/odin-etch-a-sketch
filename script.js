db47b3c76730773fd56113b9034b838b35b93284
David Islam <david.islam24@gmail.com>
2022-08-05 15:18:51 -0700

    Create square divs

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// TODO: Create 256 square divs
-const containerDiv = document.querySelector(".container");
+const containerDiv = document.querySelector('#container');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const squareDiv = document.createElement('div');
+        squareDiv.classList.add('square'); 
+        containerDiv.appendChild(squareDiv);
+    }
+}
