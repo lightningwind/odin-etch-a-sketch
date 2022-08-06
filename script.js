75d9fed8448ad071a37f5c79c5c9a31369f019d8
David Islam <david.islam24@gmail.com>
2022-08-06 07:26:50 -0700

    Gridify square divs

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const containerDiv = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const squareDiv = document.createElement('div');
-        squareDiv.classList.add('square'); 
-        containerDiv.appendChild(squareDiv);
+function makeGrid(rows, cols) {
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", cols);
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const squareDiv = document.createElement('div');
+            squareDiv.classList.add('grid-item'); 
+            containerDiv.appendChild(squareDiv);
+        }
     }
 }
+
+makeGrid(16, 16);
