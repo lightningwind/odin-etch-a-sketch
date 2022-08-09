0fcc4991d7127f67d83a4b5dfea9a917871f520f
David Islam <david.islam24@gmail.com>
2022-08-08 20:48:46 -0700

    Add button to generate new grid

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const containerDiv = document.querySelector('#container');
+const button = document.querySelector("button");
 
 function makeGrid(rows, cols) {
     containerDiv.style.setProperty("--grid-rows", rows);
     containerDiv.style.setProperty("--grid-columns", cols);
+    containerDiv.style.width = "960px";
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             const squareDiv = document.createElement('div');
@@ -14,5 +16,21 @@ function makeGrid(rows, cols) {
         }
     }
 }
+
+function removeGrid() {
+    const grid_items = document.querySelectorAll(".grid-item");
+    grid_items.forEach(grid_item => grid_item.remove());
+}
+
+button.addEventListener("click", () => {
+    removeGrid(); 
+    const userInput = +prompt("Please enter the size of the square grid (max=100): ", "16");
+    if (userInput > 100) {
+        alert("Invalid input");
+    } else {
+        makeGrid(userInput, userInput); 
+    }
+
+})
 
 makeGrid(16, 16);
